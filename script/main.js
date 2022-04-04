6f8a7bde9c06f32323f0635a7bfc3458f2cf2e88
Plastic Tortoise <simplebinary@yahoo.com>
2022-04-04 20:47:27 +0200

    Update Statistics

--- script/main.js
+++ script/main.js
@@ -15,19 +15,35 @@ function generateQuote() {
     return quotes[index];
 }
 
-function saveQuote(quote) {
+function generateStats(quote) {
     if (!localStorage.hasOwnProperty("gwekquotes")) {
         localStorage.setItem("gwekquotes", JSON.stringify([]));
     }
 
+    if (!localStorage.hasOwnProperty("gwekquotes-dates")) {
+        localStorage.setItem("gwekquotes-dates", JSON.stringify([]));
+    }
+
+    if (!localStorage.hasOwnProperty("gwekquotes-opened")) {
+        localStorage.setItem("gwekquotes-opened", "0");
+    }
+
     let myQuotes = JSON.parse(localStorage.getItem("gwekquotes"));
+    let dates = JSON.parse(localStorage.getItem("gwekquotes-dates"));
+    let opened = JparseInt(localStorage.getItem("gwekquotes-opened")) + 1;
 
     if (!myQuotes.includes(quote)) {
         myQuotes.push(quote);
     }
 
+    if (!dates.includes(getDate())) {
+        dates.push(getDate());
+    }
+
     localStorage.setItem("gwekquotes", JSON.stringify(myQuotes));
+    localStorage.setItem("gwekquotes-dates", JSON.stringify(dates));
+    localStorage.setItem("gwekquotes-opened", opened.toString());
 }
 
 document.getElementsByClassName("quote")[0].textContent = generateQuote();
-saveQuote(generateQuote());
+generateStats(generateQuote());
--- script/statistics.js
+++ script/statistics.js
@@ -1,8 +1,43 @@
+function calculateStreak(dates) {
+    let streak = 1
+
+    for (let i = 0; i < dates.length; i++) {
+        if (dates[i] === dates[i + 1] + 86400000) {
+            streak += 1
+        } else {
+            if (streak > parseInt(localStorage.getItem("gwekquotes-max-streak"))) {
+                localStorage.setItem("gwekquotes-max-streak", streak.toString());
+            }
+
+            break;
+        }
+    }
+}
+
 if (!localStorage.hasOwnProperty("gwekquotes")) {
     localStorage.setItem("gwekquotes", JSON.stringify([]));
 }
 
-let number = JSON.parse(localStorage.getItem("gwekquotes")).length;
-let total = quotes.length;
+if (!localStorage.hasOwnProperty("gwekquotes-dates")) {
+    localStorage.setItem("gwekquotes-dates", JSON.stringify([]));
+}
 
-document.getElementsByClassName("percentage")[0].textContent = `Quotes Viewed: ${(number / total * 100).toFixed(2)}%`;
+if (!localStorage.hasOwnProperty("gwekquotes-max-streak")) {
+    localStorage.setItem("gwekquotes-max-streak", "0");
+}
+
+if (!localStorage.hasOwnProperty("gwekquotes-opened")) {
+    localStorage.setItem("gwekquotes-opened", "0");
+}
+
+
+let number = JSON.parse(localStorage.getItem("gwekquotes")).length;
+let dates = JSON.parse(localStorage.getItem("gwekquotes-dates")).reverse();
+let streak = calculateStreak(dates);
+let maxStreak = localStorage.getItem("gwekquotes-max-streak");
+let opened = localStorage.getItem("opened");
+
+document.getElementsByClassName("number")[0].textContent = `Quotes Viewed: ${number}`;
+document.getElementsByClassName("streak")[0].textContent = `Current Streak: ${streak}`;
+document.getElementsByClassName("max-streak")[0].textContent = `Max Streak: ${maxStreak}`;
+document.getElementsByClassName("opened")[0].textContent = `Times Opened: ${opened}`;
