3f4de92a81ecf242899f6d4b903695b5132def32
Plastic Tortoise <simplebinary@yahoo.com>
2022-04-04 18:17:46 +0200

    Update statistics.js

--- script/statistics.js
+++ script/statistics.js
@@ -5,4 +5,8 @@ if (!localStorage.hasOwnProperty("gwekquotes")) {
 let number = localStorage.getItem("gwekquotes").length;
 let total = quotes.length;
 
+console.log(localStorage.getItem("gwekquotes"));
+console.log(quotes)
+console.log((number / total * 100).toFixed(2))
+
 document.getElementsByClassName("percentage")[0].textContent = `${(number / total * 100).toFixed(2)}%`;
