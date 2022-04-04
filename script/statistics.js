eba0c9491b20210ddc4693776c47f271e89f9782
Plastic Tortoise <simplebinary@yahoo.com>
2022-04-04 18:20:38 +0200

    Update statistics.js

--- script/statistics.js
+++ script/statistics.js
@@ -6,7 +6,9 @@ let number = localStorage.getItem("gwekquotes").length;
 let total = quotes.length;
 
 console.log(localStorage.getItem("gwekquotes"));
-console.log(quotes)
-console.log((number / total * 100).toFixed(2))
+console.log(quotes);
+console.log(number);
+console.log(total);
+console.log((number / total * 100))
 
 document.getElementsByClassName("percentage")[0].textContent = `${(number / total * 100).toFixed(2)}%`;
