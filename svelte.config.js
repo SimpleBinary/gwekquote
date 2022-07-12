79fdf5a1940cbb4beff2e128b9002f71dd535f3c
SimpleBinary <simplebinary@yahoo.com>
2022-07-12 14:26:53 +0200

    Fix Static Adapter Error

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter(),
-	}
-};
+export default {
+  kit: {
+    adapter: adapter({
+		pages: 'build',
+		assets: 'build',
+		fallback: null,
+		precompress: false
+    }),
 
-export default config;
+    prerender: {
+      	default: true
+    }
+  }
+};
